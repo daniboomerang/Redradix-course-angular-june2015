150908fdb9725d2b801d5bec856da342f263ccce
daniboomerang <estevez.dani@gmail.com>
2015-03-01 23:15:44 +0100

    Adding pdfs, course programm and same las fixes.

--- TEMA 2/EJERCICIOS/Solucion Dani/gna.js
+++ TEMA 2/EJERCICIOS/Solucion Dani/gna.js
@@ -2,8 +2,7 @@
 var gnaUIlogic = angular.module('gnaUIlogic', []);        
 
 /* Creating a new controler for aur app */  
-gnaUIlogic.controller('gnaUIlogicCtrl', function ($scope, $interval){
-    
+gnaUIlogic.controller('gnaUIlogicCtrl', function ($scope, $interval){ 
     function generateRandomNumber (){return Math.floor((Math.random() * 10) + 1)}
     var stop;
     $scope.randomNumber;
